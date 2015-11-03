c5d7302fdabac66f9561d0b0951084ca93b5defc
Nabil <nabilchamasi@gmail.com>
2015-11-03 19:48:04 -0300

    merge productos, nabil/saul

--- src/main/java/BackingBeans/ProductosBean.java
+++ src/main/java/BackingBeans/ProductosBean.java
@@ -1,5 +1,6 @@
 package BackingBeans;
 
+import EJB.Helper.ProductoResponse;
 import EJB.Service.ProductoService;
 import EJB.Service.ProveedorService;
 import JPA.ProductoEntity;
@@ -20,6 +21,8 @@ import java.util.List;
 public class ProductosBean {
 
 	/* Variables & Dependencias */
+
+	// variables saul
 	@EJB
 	ProductoService service;
 	@EJB
@@ -33,9 +36,156 @@ public class ProductosBean {
 	private Long precioModificar;
 	private String descripcionModificar;
 	private StreamedContent file;
-	private Long proveedor;
+	private Long proveedorId;
 
 	private FacesMessage message;
+
+
+    // variables nabil
+    private List<ProductoEntity> productos;
+
+    private String descripcion;
+    private String proveedor;
+    private String stock;
+    private String precio;
+    private String byAllAttributes;
+    private String byDescripcion;
+    private String byProveedor;
+    private String byStock;
+    private String byPrecio;
+    private Integer page=1;
+    private Integer totalPages=0;
+    private ProductoResponse productoResponse;
+
+    // metodos nabil
+    public void goNextPage(){
+        page+=1;
+
+        if(page<totalPages) {
+            page += 1;
+//            productoResponse = service.getProductos(descripcion, proveedor, stock, precio,
+//                    byAllAttributes, byDescripcion, byProveedor, byStock, byPrecio, page);
+//
+//            productos = productoResponse.getEntidades();
+        }
+    }
+
+    public void goBackPage(){
+        if(page>1) {
+            page -= 1;
+            productoResponse = service.getProductos(descripcion, proveedor, stock, precio,
+                    byAllAttributes, byDescripcion, byProveedor, byStock, byPrecio, page);
+
+            productos = productoResponse.getEntidades();
+        }
+    }
+
+    public void resetPage(){
+        page = 1;
+    }
+
+
+    public List<ProductoEntity> getProductos() {
+//        byStock="52";
+//        byProveedor="Vipe";
+//        byPrecio="16778";
+
+        productoResponse = service.getProductos(descripcion, proveedor, stock, precio,
+                byAllAttributes, byDescripcion, byProveedor, byStock, byPrecio, page);
+
+        productos = productoResponse.getEntidades();
+        return productos;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public Integer getTotalPages() {
+//        if(productoResponse == null)
+//            productoResponse = service.getProductos(descripcion, proveedor, stock, precio,
+//                    byAllAttributes, byDescripcion, byProveedor, byStock, byPrecio, page);
+//
+//        totalPages = productoResponse.getMeta().getTotal_pages().intValue();
+        totalPages=2;
+
+        return totalPages;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getByAllAttributes() {
+        return byAllAttributes;
+    }
+
+    public void setByAllAttributes(String byAllAttributes) {
+        this.byAllAttributes = byAllAttributes;
+    }
+
+    public String getByDescripcion() {
+        return byDescripcion;
+    }
+
+    public void setByDescripcion(String byDescripcion) {
+        this.byDescripcion = byDescripcion;
+    }
+
+    public String getByProveedor() {
+        return byProveedor;
+    }
+
+    public void setByProveedor(String byProveedor) {
+        this.byProveedor = byProveedor;
+    }
+
+    public String getByStock() {
+        return byStock;
+    }
+
+    public void setByStock(String byStock) {
+        this.byStock = byStock;
+    }
+
+    public String getByPrecio() {
+        return byPrecio;
+    }
+
+    public void setByPrecio(String byPrecio) {
+        this.byPrecio = byPrecio;
+    }
+
+
+    // metodos saul
 
 	/* Metodos */
 	/**
@@ -89,8 +239,8 @@ public class ProductosBean {
 
 		FacesContext context = FacesContext.getCurrentInstance();
 
-		if(proveedor != null) {
-			ProveedorEntity p = proveedorService.find(proveedor.intValue(), ProveedorEntity.class);
+		if(proveedorId != null) {
+			ProveedorEntity p = proveedorService.find(proveedorId.intValue(), ProveedorEntity.class);
 			productoEntity.setProveedor(p);
 		}
 		try{
@@ -121,7 +271,7 @@ public class ProductosBean {
 		setStockModificar(null);
 		setDescripcionModificar(null);
 		setPrecioModificar(null);
-		setProveedor(null);
+		setProveedorId(null);
 	}
 
 	/**
@@ -210,11 +360,11 @@ public class ProductosBean {
 		this.file = file;
 	}
 
-	public void setProveedor(Long proveedor) {
-		this.proveedor = proveedor;
+	public void setProveedorId(Long proveedorId) {
+		this.proveedorId = proveedorId;
 	}
 
-	public Long getProveedor() {
-		return proveedor;
+	public Long getProveedorId() {
+		return proveedorId;
 	}
 }
